93ed05dcd9510f36bc8fbc565ce14876f406666d
Anton801106 <listboxx@yandex.ru>
2024-07-10 17:53:48 +0300

    Add files via upload

---
+++ pythonWW_Koshelev.py
@@ -0,0 +1,8 @@
+# "1st program"
+print((int(9 ** 0.5) * 5))
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 100.1)
+# "3rd  program"
+print(int(1234 // 52) + (5678 % 5611))
+# "4th  program"
+print(13 == 13 % 13.42 or 13 % 42.13)
